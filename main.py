4de8cbda2720283b575d2ad8d7210f94dac0ae27
Tenmayos <tenmayos_2@hotmail.com>
2025-07-06 01:31:59 +0200

    stored a reference to the display screen and applied changes on it in gameloop

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from constants import *
 def main():
     # Init and config
     pygame.init()
-    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while(True):
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        pygame.Surface.fill("black")
-        pygame.display.flip()
+        screen.fill("black")
+        screen.display.flip()
 
 
 if __name__ == "__main__":
