4729e8e6084d23c20f0c1ae3e900d197bc7e2b5e
Tenmayos <tenmayos_2@hotmail.com>
2025-07-06 01:30:36 +0200

    added a game loop with an exit event check

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    # Init and config
+    pygame.init()
+    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while(True):
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        pygame.Surface.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
