b1532c35f25a2af953038ede6c7fe47ee617eda6
Tenmayos <tenmayos_2@hotmail.com>
2025-07-06 01:40:50 +0200

    added a deltatime variable and fixed the flip() function and limited fps on the clock to 60fps

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ def main():
     # Init and config
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while(True):
+    clock = pygame.time.Clock()
+    dt = 0
+    while(True): # Game loop
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
         screen.fill("black")
-        screen.display.flip()
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
