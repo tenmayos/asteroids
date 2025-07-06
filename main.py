9e2521e77d6ac31b9dea1615d37f5e8e2b5e33e9
Tenmayos <tenmayos_2@hotmail.com>
2025-07-06 02:17:58 +0200

    called the player.update() from within the game loop

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
